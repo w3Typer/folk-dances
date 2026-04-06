2e668e54b16e7fc5847cf3b422330f00e596ca07
w3Typer <159643731+w3Typer@users.noreply.github.com>
2026-04-06 21:26:31 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-fetch("./data/folklore_dances.json")
+fetch("../data/folklore_dances.json")
   .then((response) => response.json())
   .then((data) => createPage(data))
   .catch((err) => console.error(err));
